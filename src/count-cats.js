b314f648dc0e463397e737e5e7731c859dcc7c63
Дмитрий Шергин <dmitrishergin@MacBook-Air-Dmitrij.local>
2025-11-10 20:03:09 +0300

    feat: add third

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
@@ -14,11 +14,20 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  let count = 0;
+
+  for (let row of matrix) {
+    for (let i = 0; i < row.length - 1; i++) {
+      if (row[i] === "^" && row[i + 1] === "^") {
+        count++;
+      }
+    }
+  }
+
+  return count;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
