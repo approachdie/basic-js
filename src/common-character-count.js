e073142f404ce5f02164f9a58330227c03e8ffb4
Дмитрий Шергин <dmitrishergin@MacBook-Air-Dmitrij.local>
2025-11-10 20:00:03 +0300

    feat: add second task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -12,11 +12,29 @@ const { NotImplementedError } = require('../lib');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 
-function getCommonCharacterCount(/* s1, s2 */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const arr1 = {};
+  const arr2 = {};
+
+  for (const char of s1) {
+    arr1[char] = (arr2[char] || 0) + 1;
+  }
+
+  for (const char of s2) {
+    arr1[char] = (arr2[char] || 0) + 1;
+  }
+
+  let count = 0;
+
+  for (const char in freq1) {
+    if (arr2[char]) {
+      count += Math.min(arr1[char], arr2[char]);
+    }
+  }
+
+  return count;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
