e9ec07d7282a54d1773ab135d588110822428ede
Дмитрий Шергин <dmitrishergin@MacBook-Air-Dmitrij.local>
2025-11-10 20:17:09 +0300

    feat: add seventh

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given a string, return its encoding version.
@@ -11,11 +11,22 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  let encoded = "";
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      encoded += (count > 1 ? count : "") + str[i];
+      count = 1;
+    }
+  }
+
+  return encoded;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
